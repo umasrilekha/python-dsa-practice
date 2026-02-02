bb0639e41e304b223c5572dcb930b00e4e24fe60
Jandhyala Uma Sri Lekha <umasrilekhaj@gmail.com>
2026-02-02 11:42:37 +0530

    Add loop to print squares of numbers
    Implement loop to print squares of numbers.

---
+++ Python/basics/loops.py
@@ -0,0 +1,8 @@
+# Platform: HackerRank
+# Problem: To print square of each number in seperate line
+# Topic: Loops
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
